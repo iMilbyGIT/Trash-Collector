e172731e1ba01320147be8b191640bec0ca89c12
Adam <dev.adammilbrath@gmail.com>
2020-01-06 15:33:31 -0600

    created customer and employee models/classes

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        
+        [Key]
+        public int Id { get; set; }
+        public string PickupDay { get; set; }
+        public string FirstName { get; set; }
+        public string LastDay { get; set; }
+        public int ExtraPickupDate { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public double Balance { get; set; }
+        public string SuspendedStart { get; set; }
+        public string SuspendedEnd { get; set; }
+        public bool PickupConfirm { get; set; }
+
+
+    }
+}
---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
