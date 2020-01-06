39ac836df5ae814658cf8070fe776a3014178927
Adam <dev.adammilbrath@gmail.com>
2020-01-06 14:43:24 -0600

    Init

---
+++ TrashCollector/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrashCollector.MvcApplication" Language="C#" %>
---
+++ TrashCollector/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashCollector.Startup))]
+namespace TrashCollector
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
